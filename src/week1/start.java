fb8e89508d19784b5604e0030321f21143faa14c
Aleks <becharal@sheridancollege.ca>
2022-01-18 14:03:40 -0500

    Checkpoint 1 - added the introduction to the toc and began writing the introduction part

--- src/week1/start.java
+++ src/week1/start.java
@@ -6,6 +6,18 @@ public class start
 {
    public static void main (String[] args)
    {
+      student[] list = new student[4];
+
+      list[0] = new student("s1", "Aleks Bechard");
+      list[1] = new student("s2", "Tina Beclcher");
+      list[2] = new student("s3", "Bob Belcher");
+      list[3] = new student("s4", "Linda Belcher");
+
+      for (int i = 0; i < list.length; i++) {
+         System.out.println(list[i].getStudentId() + "  " + list[i].getStudentName() + "  ");
+
+      }
+
 
    }
 }
--- src/week1/student.java
+++ src/week1/student.java
@@ -1,8 +1,36 @@
 package week1;
 /**
- * Aleks Bechard  
+ * Aleks Bechard
  */
-public class student 
+public class student
 {
+   private String studentId;
+   private String studentName;
+
+   public student (String studentId, String studentName)
+   {
+      this.studentId = studentId;
+      this.studentName = studentName;
+   }
+
+   public void setStudentId (String studentId)
+   {
+      this.studentId = studentId;
+   }
+
+   public void setStudentName (String studentName)
+   {
+      this.studentName = studentName;
+   }
+
+   public String getStudentId ()
+   {
+      return studentId;
+   }
+
+   public String getStudentName ()
+   {
+      return studentName;
+   }
 
 }
